c890f174b8abdbf37a2eb8d1c8e75a30e023bfa6
Nemanja Rajic <55772835+nemanjarajic@users.noreply.github.com>
2021-03-10 21:27:07 -0800

    Update permutations.cpp

--- permutations.cpp
+++ permutations.cpp
@@ -1,3 +1,8 @@
+/*
+A permutation of integers 1,2,…,n is called beautiful if there are no adjacent elements whose difference is 1.
+
+Given n, construct a beautiful permutation if such a permutation exists.
+*/
 #include <iostream>
 using namespace std;
 
